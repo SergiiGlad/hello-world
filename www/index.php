48f8332e136ca059932081d048f4f60f1cd3b00c
sergiiglad <sergii.gladchenko@gmail.com>
2018-10-01 13:58:52 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Softserve!</title>
+	<title>hello!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Demos ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<h1><?php echo "Demka three ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
