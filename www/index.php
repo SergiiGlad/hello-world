3f6eecb34e659a9680496a36f37db582ab877e72
sergiiglad <sergii.gladchenko@gmail.com>
2018-10-01 12:51:13 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello softserve!</title>
+	<title>Softserve!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
